10f75e9e087fee19cd72040139857135c3e4239d
jjimmanuel <99039852+jjimmanuel@users.noreply.github.com>
2025-07-29 22:15:39 -0400

    Update cir.cu

--- cir.cu
+++ cir.cu
@@ -33,6 +33,7 @@ __global__ void cir(curandState *states, float *results, float kappa, float thet
 	float r = r0;
 	results[idx * (numSteps + 1)] = r;
 
+	
 	for (int i = 0; i < numSteps; ++i) {
 		float c = (sigma * sigma * (1.0 - exp(-kappa * dt))) / (4.0 * kappa);
 		float d = (4.0 * kappa * theta) / (sigma * sigma);
