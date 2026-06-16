eb5faaad722d51458be9cbb3bee2ed13d4dddbbc
jjimmanuel <99039852+jjimmanuel@users.noreply.github.com>
2026-06-16 12:02:16 -0400

    Enhance documentation with Javadoc comments
    Added Javadoc comments and documentation for the FlexibleNotificationSystem class.

--- FlexibleNotificationSystem.java
+++ FlexibleNotificationSystem.java
@@ -1,3 +1,11 @@
+/** 
+ * The underlying Java code was written by me without the assistance of GenAI. 
+ * The reason for this was because I wanted more practice with Java, as I mainly use C#, which is Java-esque
+ * However, I did use GenAI to generate comments and Javadoc tags. I queried the AI with the following query:
+ * "Write descriptive Javadoc comments for the following code: [pasted code]. Do not alter or edit the underlying code."
+ * I made some edits to the GenAI output, chief of which were the example notifications which were strangely specific, referencing random topics
+ * For the WhatsAppService addition, I also didn't use GenAI because it was a copy/paste of the Email/SMS notification class, as deskgned. 
+*/
 import java.util.ArrayList;
 import java.util.List;
 
